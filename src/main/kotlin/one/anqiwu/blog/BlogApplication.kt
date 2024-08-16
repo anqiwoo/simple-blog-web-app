e4d295c4fa62c0cfeefdf740e093a1d4e3feadec
Anqi Wu <anqiwu85@outlook.com>
2024-08-16 09:19:37 -0700

    Set up project

---
+++ src/main/kotlin/one/anqiwu/blog/BlogApplication.kt
@@ -0,0 +1,11 @@
+package one.anqiwu.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootAppliechcation
+class BlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args)
+}
